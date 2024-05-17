b4a2498776e460355420bf341c783edc4f2eb9a5
BradleyRodriq <bradley.rodriguez2@yahoo.com>
2024-05-16 21:03:56 -0400

    task 3

--- javascript-web_scraping/3-starwars_title.js
+++ javascript-web_scraping/3-starwars_title.js
@@ -1,23 +1,6 @@
 #!/usr/bin/node
 const request = require('request');
-
-const filmId = process.argv[2];
-if (!filmId) {
-  console.error('Error: No film ID provided.');
-  process.exit(1);
-}
-
-const url = 'https://swapi.dev/api/films/' + filmId + '/';
-
+let url = 'http://swapi-api.hbtn.io/api/films/' + process.argv[2];
 request(url, function (error, response, body) {
-  if (error) {
-    console.error('Error:', error);
-    process.exit(1);
-  }
-  try {
-    const data = JSON.parse(body);
-    console.log(data.title);
-  } catch (e) {
-    console.error('Error parsing JSON:', e);
-  }
+  console.log(error || JSON.parse(body).title);
 });
